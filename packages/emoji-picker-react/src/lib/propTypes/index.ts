c097bd0448e02fb17ed6c003417df06817c93ad3
Amit Dahan <amitdahan11@gmail.com>
2021-12-29 20:26:14 +0000

    More types in propTypes/index.ts

--- packages/emoji-picker-react/src/lib/propTypes/index.ts
+++ packages/emoji-picker-react/src/lib/propTypes/index.ts
@@ -1,17 +1,16 @@
-import PropTypes from 'prop-types';
 import {
   EMOJI_PROPERTY_NAME,
   EMOJI_PROPERTY_UNIFIED,
   EMOJI_PROPERTY_SKIN_VARIATIONS,
   EMOJI_PROPERTY_GROUP,
-} from '../../../lib/constants';
+} from '../constants';
 
-export const emoji = PropTypes.shape({
-  [EMOJI_PROPERTY_NAME]: PropTypes.arrayOf(PropTypes.string),
-  [EMOJI_PROPERTY_UNIFIED]: PropTypes.string,
-  [EMOJI_PROPERTY_SKIN_VARIATIONS]: PropTypes.arrayOf(PropTypes.string),
-  [EMOJI_PROPERTY_GROUP]: PropTypes.string,
-});
+export interface EmojiProps {
+  [EMOJI_PROPERTY_NAME]?: string[];
+  [EMOJI_PROPERTY_UNIFIED]?: string;
+  [EMOJI_PROPERTY_SKIN_VARIATIONS]?: string[];
+  [EMOJI_PROPERTY_GROUP]?: string;
+}
 
 export interface GroupNamesProps {
   smileys_people?: string;
